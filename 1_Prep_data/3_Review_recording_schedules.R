62b8b768437f9e2ee937296a9e234188fd616c9a
Jeremiah Kennedy <jeremiah.kennedy@ausenco.com>
2023-06-10 09:13:50 -0700

    New recordiong schedule script

--- 1_Prep_data/3_Review_recording_schedules.R
+++ 1_Prep_data/3_Review_recording_schedules.R
@@ -14,11 +14,26 @@
 dev.off()
 rm(list=ls())
 
+# library what is needed
+library(stringr)
+
 # Base folder with recordings present 
 prnt.source = c("S:/ProjectScratch/398-173.07/PMRA_WESOke/PMRA_SAR/2023_WLRS_Contract/recordings/2022_Nawhitti")
 
 #### First trial of this component of script ######
 
-# set intermediate directories 
-all.dirs = list.dirs(prnt.source,recursive = T,full.names = T)
+# set intermediate directories
+## IMPORTANT NOTE: This won't work if you don't at least have all recordigns kept in the same depth of directories throughout this parent directory 
+
+all.dirs = data.frame(Full = list.dirs(prnt.source,recursive = T,full.names = T)) # full names of directories present
+all.dirs$depth <- lengths(strsplit(all.dirs$Full, "/")) # get directory depth 
+all.dirs = all.dirs[all.dirs$depth == max(all.dirs$depth),]# keep only the deepest directories  
+all.dirs$int.dirs = gsub(prnt.source,"",all.dirs$Full)
+all.dirs$int.length = str_count(all.dirs$int.dirs,"/")
+
+
+### C
+
+
+
 
